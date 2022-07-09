ccdd0a24894a222192034fa6a1d761847da574a2
Aleydon <aleydonroberto@gmail.com>
2022-07-09 11:52:57 -0300

    Set ThemeProvider in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
+import { ThemeProvider } from 'styled-components';
+
 import GlobalStyles from './styles/GlobalStyles';
+import dark from './styles/themes/dark';
 import Layout from './components/Layout';
 
 export default function App() {
   return (
-    <>
+    <ThemeProvider theme={dark}>
       <GlobalStyles />
       <Layout />
-    </>
+    </ThemeProvider>
   );
 }
