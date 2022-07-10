b37b6fd62750adb380e32832d2b3443fd7211957
Aleydon <aleydonroberto@gmail.com>
2022-07-10 11:40:15 -0300

    Set styles and emoji in Header

--- src/components/MainHeader/index.tsx
+++ src/components/MainHeader/index.tsx
@@ -1,9 +1,21 @@
-import Container from './style';
+import { useMemo } from 'react';
+
+import Container, { Profile, Welcome, Username } from './style';
+import emojis from '../../utils/emojis';
 
 const MainHeader = () => {
+  const emoji = useMemo(() => {
+    const index = Math.floor(Math.random() * emojis.length);
+    return emojis[index];
+  }, []);
+
   return (
     <Container>
-      <h5>MainHeader</h5>
+      <h5>Toggle Theme</h5>
+      <Profile>
+        <Welcome>Hello, {emoji}</Welcome>
+        <Username>Roberto Aleydon</Username>
+      </Profile>
     </Container>
   );
 };
--- src/components/MainHeader/style.ts
+++ src/components/MainHeader/style.ts
@@ -1,10 +1,21 @@
 import styled from 'styled-components';
 
 const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0px 10px;
+  border-bottom: solid 1px ${props => props.theme.color.gray};
   grid-area: MH;
   color: ${props => props.theme.color.white};
   background-color: ${props => props.theme.color.secondary};
-  height: 100%;
 `;
+
+export const Profile = styled.div`
+  color: ${props => props.theme.color.white};
+  display: flex;
+`;
+export const Welcome = styled.h3``;
+export const Username = styled.span``;
 
 export default Container;
